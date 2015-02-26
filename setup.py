dfa9b006a4e07bbf40277b5a0c15cf46810a65ca
Omid Khosravani <okhosravani@berkeley.edu>
2015-02-25 23:47:55 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ion-trap-qsim',
+      version='1.0',
+      description='Ion Trap QSim is a package for simulation of quantum and classical dynamics of trapped ion chains in interaction with lasers and trap electromagnetic fields.',
+      url='https://github.com/HaeffnerLab/ion-trap-qsim',
+      author='Omid Khosravani',
+      author_email='okhosravani@gmail.com',
+      license='MIT',
+      packages=['ion-trap-qsim'],
+      zip_safe=False)
